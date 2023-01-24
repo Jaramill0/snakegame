1829e6e0d09676f477633fa7af5dede28412939c
Jaramill0 <hugo98jaramillo@gmail.com>
2023-01-23 20:50:13 -0600

    Simplificacion del codigo

--- main/main.cpp
+++ main/main.cpp
@@ -113,7 +113,6 @@ void Logic() {
 		prevX = prev2X;
 		prevY = prev2Y;
 
-
 	}
 	//se inserta un switch para las instrucciones del juego y la movilidad de la serpiente en la consola 
 	switch (dir)
