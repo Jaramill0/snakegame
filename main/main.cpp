fcb1ee12f0343d428186161f87be23938aa17bd8
Jaramill0 <hugo98jaramillo@gmail.com>
2023-01-23 20:37:46 -0600

    se integran las libreria de C++

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,6 @@
 // gamesnake.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 // Dev  The Young 
-
+//se integran las librerias para la compilacion de juego
 #include "pch.h"
 #include <iostream>
 #include <conio.h>
